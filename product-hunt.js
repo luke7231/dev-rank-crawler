ca694dd9994b7fda826e532281ae7523139e707e
luke7231 <tmddnjs7231@gmail.com>
2024-04-21 01:48:34 +0900

    feat: product-hunt 크롤러

---
+++ product-hunt.js
@@ -0,0 +1,35 @@
+const chrome = require("selenium-webdriver/chrome");
+const { By } = require("selenium-webdriver");
+
+const run = async () => {
+  let service = new chrome.ServiceBuilder()
+    .loggingTo("./chromedriver.exe")
+    .enableVerboseLogging()
+    .build();
+  let options = new chrome.Options();
+  let driver = chrome.Driver.createSession(options, service);
+
+  const url = "https://www.producthunt.com/";
+  await driver.get(url);
+
+  const productHuntTop5 = await driver.findElements(
+    By.xpath("//div[starts-with(@class,'styles_titleContainer')]")
+  );
+  //   console.log(careerlyTop5);
+  const TitleAndLinks = await Promise.all(
+    productHuntTop5.slice(0, 5).map(async (tag, index) => {
+      const title = await tag
+        .findElement(By.tagName("strong")) // strong 을 쓰더라.
+        .getText();
+      const link = await tag.findElement(By.tagName("a")).getAttribute("href");
+      // .findElement(By.xpath(".//a[starts-with(@href, '/posts')]")) // 이거 왜 안되지?
+      return { rank: index + 1, title, link };
+    })
+  );
+  console.log(TitleAndLinks);
+
+  //   console.log("total count : " + TitleAndLinks.length);
+  driver.quit();
+};
+
+run();
