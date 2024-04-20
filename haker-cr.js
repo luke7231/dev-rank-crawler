7696f2a003d6fdbf238a023e2f980157bc0a3d9f
luke7231 <tmddnjs7231@gmail.com>
2024-04-21 00:05:10 +0900

    feat: 해커뉴스 크롤러

---
+++ haker-cr.js
@@ -0,0 +1,35 @@
+const chrome = require("selenium-webdriver/chrome");
+const { By } = require("selenium-webdriver");
+
+const run = async () => {
+  let service = new chrome.ServiceBuilder()
+    .loggingTo("./chromedriver.exe")
+    .enableVerboseLogging()
+    .build();
+  let options = new chrome.Options();
+  let driver = chrome.Driver.createSession(options, service);
+
+  const url = "https://news.ycombinator.com/";
+  await driver.get(url);
+
+  const hakerRankingTop5 = (
+    await driver.findElements(By.className("athing"))
+  ).slice(0, 5);
+
+  const TitleAndLinks = await Promise.all(
+    hakerRankingTop5.map(async (tag) => {
+      const title = await tag.findElement(By.className("titleline")).getText();
+      const link = await tag
+        .findElement(By.className("titleline"))
+        .findElement(By.tagName("a"))
+        .getAttribute("href");
+      return { title, link };
+    })
+  );
+  console.log(TitleAndLinks);
+
+  console.log("total count : " + TitleAndLinks);
+  driver.quit();
+};
+
+run();
